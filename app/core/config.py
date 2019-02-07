cb170ea5d7373673a8bf7917ee1e42f0d26ed74f
Nik Sidnev <snive2013@yandex.ru>
2019-02-07 05:19:00 +0300

    config from starlette replaced with dotenv for simplicity

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 import os
 
-from starlette.config import Config
+from dotenv import load_dotenv
 from starlette.datastructures import CommaSeparatedStrings, DatabaseURL, Secret
 
 API_V1_STR = "/api"
@@ -8,27 +8,25 @@ API_V1_STR = "/api"
 JWT_TOKEN_PREFIX = "Token"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
 
-config = Config(".env")
+load_dotenv(".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")  # deploying without docker-compose
 if not DATABASE_URL:
-    POSTGRES_HOST = config("POSTGRES_HOST")
-    POSTGRES_PORT = config("POSTGRES_PORT")
-    POSTGRES_USER = config("POSTGRES_USER")
-    POSTGRES_PASS = config("POSTGRES_PASSWORD")
-    POSTGRES_NAME = config("POSTGRES_DB")
-    MAX_CONNECTIONS_COUNT = config("MAX_CONNECTIONS_COUNT", default=10, cast=int)
-    MIN_CONNECTIONS_COUNT = config("MIN_CONNECTIONS_COUNT", default=10, cast=int)
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", 5432))
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASS = os.getenv("POSTGRES_PASSWORD", "postgres")
+    POSTGRES_NAME = os.getenv("POSTGRES_DB", "postgres")
 
     DATABASE_URL = DatabaseURL(
         f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_NAME}"
     )
-    SECRET_KEY = config("SECRET_KEY", cast=Secret)
 else:
     DATABASE_URL = DatabaseURL(DATABASE_URL)
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
-    MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
 
-PROJECT_NAME = config("PROJECT_NAME", default="FastAPI example application")
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings, default=None)
+MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
+MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
+SECRET_KEY = Secret(os.getenv("SECRET_KEY", "secret key for project"))
+
+PROJECT_NAME = os.getenv("PROJECT_NAME", "FastAPI example application")
+ALLOWED_HOSTS = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", ''))
