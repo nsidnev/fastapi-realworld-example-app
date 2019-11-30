07c1da622b6a5802b910f5b423dd0c576081cf78
Nik Sidnev <sidnev.nick@gmail.com>
2019-11-30 23:38:40 +0300

    fix typings

--- app/core/events.py
+++ app/core/events.py
@@ -6,14 +6,14 @@ from loguru import logger
 from app.db.events import close_db_connection, connect_to_db
 
 
-def create_start_app_handler(app: FastAPI) -> Callable[[], None]:
+def create_start_app_handler(app: FastAPI) -> Callable:  # type: ignore
     async def start_app() -> None:
         await connect_to_db(app)
 
     return start_app
 
 
-def create_stop_app_handler(app: FastAPI) -> Callable[[], None]:
+def create_stop_app_handler(app: FastAPI) -> Callable:  # type: ignore
     @logger.catch
     async def stop_app() -> None:
         await close_db_connection(app)
