56e7835ed2b32aa51cbcc9692805c28947eafb08
Nik <snive2013@yandex.ru>
2020-05-01 12:31:44 +0300

    setup InterceptHandler more properly to handle uvicorn logs (#36)

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,9 +1,24 @@
 import logging
+from types import FrameType
+from typing import cast
 
 from loguru import logger
 
 
 class InterceptHandler(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:  # pragma: no cover
-        logger_opt = logger.opt(depth=7, exception=record.exc_info)
-        logger_opt.log(record.levelname, record.getMessage())
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = str(record.levelno)
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
+            frame = cast(FrameType, frame.f_back)
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
