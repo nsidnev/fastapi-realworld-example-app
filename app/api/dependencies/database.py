6c9d06448839e3977b5c3e2a274457fa5a6eb34c
Nik Sidnev <sidnev.nick@gmail.com>
2020-07-01 12:43:50 +0300

    acquire connection from db pool once per request

--- app/api/dependencies/database.py
+++ app/api/dependencies/database.py
@@ -1,5 +1,6 @@
 from typing import AsyncGenerator, Callable, Type
 
+from asyncpg.connection import Connection
 from asyncpg.pool import Pool
 from fastapi import Depends
 from starlette.requests import Request
@@ -11,11 +12,19 @@ def _get_db_pool(request: Request) -> Pool:
     return request.app.state.pool
 
 
-def get_repository(repo_type: Type[BaseRepository]) -> Callable:  # type: ignore
-    async def _get_repo(
-        pool: Pool = Depends(_get_db_pool),
-    ) -> AsyncGenerator[BaseRepository, None]:
-        async with pool.acquire() as conn:
-            yield repo_type(conn)
+async def _get_connection_from_pool(
+    pool: Pool = Depends(_get_db_pool),
+) -> AsyncGenerator[Connection, None]:
+    async with pool.acquire() as conn:
+        yield conn
+
+
+def get_repository(
+    repo_type: Type[BaseRepository],
+) -> Callable[[Connection], BaseRepository]:
+    def _get_repo(
+        conn: Connection = Depends(_get_connection_from_pool),
+    ) -> BaseRepository:
+        return repo_type(conn)
 
     return _get_repo
